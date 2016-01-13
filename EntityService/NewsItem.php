422c4136992b119d114b724ccb60c98eabf5a7f5
tico86 <timo.collmann@ymc.ch>
2016-01-13 12:17:51 +0100

    #CE-527
    Remove operation

--- EntityService/NewsItem.php
+++ EntityService/NewsItem.php
@@ -44,4 +44,13 @@ class NewsItem implements OperationServiceInterface
         $this->em->persist($clonedNewsItem);
         $this->em->flush();
     }
+    public function removeOperation($id){
+        try {
+            $operation = $this->getNewsItemByOperation($id);
+            $this->em->remove($operation);
+            $this->em->flush();
+        } catch (\Exception $e) {
+
+        }
+    }
 }
